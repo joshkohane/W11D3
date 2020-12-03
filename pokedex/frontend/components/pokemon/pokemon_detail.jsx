62d8bb449648c9a6ead76dc26ef6546b1ee36e1a
joshkohane <joshkohane@gmail.com>
2020-12-02 20:06:00 -0500

    Set up routes for pokedex

---
+++ pokedex/frontend/components/pokemon/pokemon_detail.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class PokemonDetail extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        this.props.requestPokemon(this.props.match.params.pokemonId);
+    }
+
+    render() {
+        // if (!pokemon) return null;
+        const { pokemon, moves, items } = this.props;
+
+        // const listMoves = [];
+        // moves.forEach(move => listMoves.push(move.name));
+        return (
+            <section className="pokedex">
+                <img src={pokemon.imageUrl} />
+                {pokemon.name}
+                Type: {pokemon.poke_type}
+                Attack: {pokemon.attack}
+                Defense: {pokemon.defense}
+                Moves: {/*listMoves.join(', ')*/}
+
+            </section>
+        );
+    }
+}
+
+export default PokemonDetail;
---
+++ pokedex/frontend/reducers/selectors.js
@@ -0,0 +1,4 @@
+export const selectAllPokemon = (state) => (
+    // { pokemon: { 1: { ... }, 2: ... }}
+    Object.values(state.entities.pokemon)
+);
